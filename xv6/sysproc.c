5f6b8049c68a5c3014c3fcf99e38dc46ed5c6c75
Miguelangel Tinoco <mtino006@sledge.cs.ucr.edu>
2023-02-23 12:19:58 -0800

    Adding lab 2 files

---
+++ xv6/sysproc.c
@@ -0,0 +1,133 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  int *status;
+  if(argptr(0, (void*)&status, sizeof(status)) < 0) { 
+    return -1; 
+  }
+  return wait(status); 
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+//LAB1  
+int  
+sys_exitStat(int status){
+    argint(0, &status); 
+    exitStat(status);
+    return 0; 
+}
+
+
+int
+sys_waitpid(void)
+{ 
+  int pid;
+  int options = 0;
+  int* status;
+  if(argint(0, &pid) < 0){
+    return -1;
+  }
+  if(argptr(1,(void*)&status, sizeof(status)) < 0){ 
+    return -1; 
+  }
+  return waitpid(pid, status, options);
+}
+//LAB 2
+int sys_setPriority(void)
+{
+    int priority;
+    if (argint(0,&priority)<0){
+        return -1;
+    }
+    return setPriority(priority);
+}
+
+int sys_getPriority(void)
+{
+    return getPriority();
+}
